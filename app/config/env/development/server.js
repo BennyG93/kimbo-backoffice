64a8aecfb01f33f5ab7d3ed0a09976cd7aaafe5e
Ben Groves <benjamin.groves@letgo.com>
2021-03-24 18:32:43 +0100

    updating default url to be more flexible

--- app/config/env/development/server.js
+++ app/config/env/development/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('SERVER_URL', 'http://localhost:1337'),
+  url: env('SERVER_URL', ''),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'b70948ff8dc22013cc369d1ffafa7d58'),
