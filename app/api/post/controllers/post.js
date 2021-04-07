49c1e6273a1bfad0b6c50041d4d43d866c273245
Ben Groves <benjamin.groves@letgo.com>
2021-04-07 17:20:00 -0300

    adding new route for fetching and returning all places in order of distance to a set of coords

--- app/api/post/controllers/post.js
+++ app/api/post/controllers/post.js
@@ -1,8 +1,40 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
+const { orderByDistance } = require('geolib');
+// import { orderByDistance } from 'geolib';
+
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
+ * 
+ * local() is a new custom controller which does not come with standard strapi, its purpose is to query for all results
+ * then order the results by nearest distance to a specific set of coordinates.
  */
 
-module.exports = {};
+
+module.exports = {
+  async local(ctx) {
+    let entities;
+    if (ctx.query._q) {
+        entities = await strapi.services.post.search(ctx.query);
+    } else if (ctx.query.longitude && ctx.query.latitude) {
+        const location = { // store coords in new object
+            longitude: ctx.query.longitude,
+            latitude: ctx.query.latitude
+        }
+        // delete the coord values from the original query object so it doesnt affect the results, all other queries will remain
+        delete ctx.query.longitude;
+        delete ctx.query.latitude;
+
+        const unordered_entities = await strapi.services.post.find(ctx.query); // fetch the results (without the coords)
+        // order the results using geolib library
+        entities = orderByDistance({ latitude: location.latitude, longitude: location.longitude }, unordered_entities);
+    } else {
+        console.log('third catch')
+        entities = await strapi.services.post.find(ctx.query);
+    }
+
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.post }));
+  },
+};
